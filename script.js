5b096d020ee9727cd8d81e12604fe95f18d6fb08
nabeel nizar <nabeelnizar786@gmail.com>
2024-01-13 12:55:10 +0530

    finalcommit

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 function cursorChange () {
     const cursor = document.querySelector('.cursor')
+    const links = document.querySelectorAll('a')
+    const articles = document.querySelectorAll('.articles')
+
     document.addEventListener('mousemove', (event)=> {
         let xPos =  event.clientX
         let yPos = event.clientY
 
         cursor.style.transform = `translate3d(calc(${xPos}px - 50%),calc(${yPos}px - 50%),0)`
+    })
+
+    links.forEach((link) => {
+        link.addEventListener('mouseover',()=>{
+            cursor.classList.add('hovered')
+        })
+        link.addEventListener('mouseleave', () => {
+            cursor.classList.remove('hovered')
+        })
+    })
+
+    articles.forEach((article) => {
+        article.addEventListener('mouseover',()=>{
+            cursor.classList.add('hovered')
+        })
+        article.addEventListener('mouseleave',()=>{
+            cursor.classList.remove('hovered')
+        })
     })
 }
 
